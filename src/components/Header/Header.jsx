4fe4a9a74e8f341098a89e8df5db08f3509884d6
manoj-thomas-sainsburys <manoj.thomas@sainsburys.co.uk>
2022-04-09 15:18:43 +0100

    Add header links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { useBasketContext } from '../../context/Basket'
 
 var formatterGB = new Intl.NumberFormat('en-GB', {
@@ -7,12 +8,17 @@ var formatterGB = new Intl.NumberFormat('en-GB', {
 
 const Header = () => {
   const { products } = useBasketContext()
-  const productCount = products.length
+  const productCount = products.reduce((accum, curr) => curr.quantity + accum, 0)
   const productTotal = products.reduce((accum, curr) => curr.price * curr.quantity + accum, 0)
 
   return (
     <div>
-      {productCount} {formatterGB.format(productTotal)}
+      <Link href="/">Home</Link>
+      <Link href="/basket">
+        <a>
+          {productCount} {formatterGB.format(productTotal)}
+        </a>
+      </Link>
     </div>
   )
 }
