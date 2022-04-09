cb7ada28923f6db37940cd0dc378320d333d0cd4
manoj-thomas-sainsburys <manoj.thomas@sainsburys.co.uk>
2022-04-09 12:37:55 +0100

    Add image domain to config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['images.hollandandbarrettimages.co.uk'],
+  },
 }
 
 module.exports = nextConfig
