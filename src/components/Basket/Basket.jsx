da1922d19b7c5c04edccdadf55c14a6a2021efea
manoj-thomas-sainsburys <manoj.thomas@sainsburys.co.uk>
2022-04-10 13:52:01 +0100

    Add basic styling for basket list

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import { useBasketContext } from '../../context/Basket'
 import ProductCounter from '../ProductCounter'
 import formatCurrency from '../../utils/formatCurrency'
@@ -14,17 +16,20 @@ const Basket = () => {
       <div className={styles.products}>
         {products.map(product => {
           return (
-            <div key={product.id}>
-              <div> {product.title}</div>
-              <div> {product.price}</div>
+            <div key={product.id} className={styles.productCard}>
+              <Image alt={product.title} src={product.image} width={180} height={180} />
+              <div>
+                <div> {product.title}</div>
+                <div> {formatCurrency(product.price)}</div>
 
-              <ProductCounter
-                value={product.quantity}
-                onChange={qty => update(product.id, qty)}
-                onAdd={() => add(product)}
-                onRemove={() => remove(product.id)}
-                onDelete={() => remove(product.id, true)}
-              />
+                <ProductCounter
+                  value={product.quantity}
+                  onChange={qty => update(product.id, qty)}
+                  onAdd={() => add(product)}
+                  onRemove={() => remove(product.id)}
+                  onDelete={() => remove(product.id, true)}
+                />
+              </div>
             </div>
           )
         })}
